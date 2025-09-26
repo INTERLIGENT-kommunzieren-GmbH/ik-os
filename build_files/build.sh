e0e2fadce94444cb953ca4baf0a39f8ae85012b0
Tito Duarte <tito.duarte@interligent.com>
2025-09-26 19:28:51 +0100

    updated iso info

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,7 @@ set -ouex pipefail
 # this installs a package from fedora repos
 #dnf5 install -y epson-inkjet-printer-escpr2
 rpm -i /ctx/epson-inkjet-printer-escpr-1.8.6-1.x86_64.rpm
+rpm -i /ctx/QualysCloudAgent.rpm
 
 # Use a COPR Example:
 #
