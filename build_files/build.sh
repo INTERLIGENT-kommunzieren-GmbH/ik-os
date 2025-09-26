9c5e18872f29c92d9c2be74acde4eadf22c77c01
Tito Duarte <tito.duarte@interligent.com>
2025-09-26 21:01:05 +0100

    added mc

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -e
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-#dnf5 install -y epson-inkjet-printer-escpr2
+dnf5 install -y mc
 rpm -i /ctx/epson-inkjet-printer-escpr-1.8.6-1.x86_64.rpm
 mkdir -p /var/usrlocal/qualys/cloud-agent/bin
 mkdir -p /var/usrlocal/qualys/cloud-agent/data
