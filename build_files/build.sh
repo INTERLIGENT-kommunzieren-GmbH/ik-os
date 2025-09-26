3c8f7506ac5f1ec2464786cf86739400174fde64
Tito Duarte <tito.duarte@interligent.com>
2025-09-26 20:36:02 +0100

    updated qualys agent

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,10 +12,9 @@ set -e
 # this installs a package from fedora repos
 #dnf5 install -y epson-inkjet-printer-escpr2
 rpm -i /ctx/epson-inkjet-printer-escpr-1.8.6-1.x86_64.rpm
-stat /usr/local
-mkdir -p /usr/local/qualys/cloud-agent/bin
-mkdir -p /usr/local/qualys/cloud-agent/data
-mkdir -p /usr/local/qualys/cloud-agent/data/manifests
+mkdir -p /var/usrlocal/qualys/cloud-agent/bin
+mkdir -p /var/usrlocal/qualys/cloud-agent/data
+mkdir -p /var/usrlocal/qualys/cloud-agent/data/manifests
 mkdir -p /etc/qualys/cloud-agent
 mkdir -p /etc/qualys/cloud-agent-defaults
 mkdir -p /var/log/qualys
